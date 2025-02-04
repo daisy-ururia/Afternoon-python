c8a76741a0af522ab2c7e1eb98e86cc08872a53d
daisy-ururia <uluriadaisy@gmail.com>
2025-02-03 20:05:12 -0800

    initial commit

---
+++ introduction.py
@@ -0,0 +1,5 @@
+#print is a method used to display
+print("Daisy")
+print("eMobilis")
+print("100")
+print(10+20)
---
+++ variable.py
@@ -0,0 +1,17 @@
+#variable is a container used to store a value
+location ="westlands"
+firstname ="Daisy"
+lastname ="Ururia"
+school ="eMobilis"
+x =39
+y =2
+
+print(location)
+
+print(firstname)
+print(lastname)
+print(school)
+print(firstname,lastname)
+print(school,"is a coding school")
+#the product of 39 and 2 is 78
+print("The product of", x ,"and", y ,"is",x*y)
